dbbc42d6cf430ed7468f6cd316dd52a7fc04ae7f
manavpatel1021 <84863486+manavpatel1021@users.noreply.github.com>
2021-05-27 23:00:32 +0530

    day 2
    basics of php like loop and switch case

---
+++ index.php
@@ -0,0 +1,23 @@
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+
+$a = 2;
+
+switch ($a)
+{
+    case 1:
+        echo 'one';
+        break;
+    
+    case 2:
+        echo 'two';
+        break;
+    
+    default :
+        echo 'please select 1 or 2';
+}
+?>
---
+++ loop.php
@@ -0,0 +1,23 @@
+<?php
+echo "in the for loop <br>";
+for($a = 0 ; $a < 5 ; $a++)
+{
+    echo "$a";
+    echo "<br>";
+}
+$i = 0;
+echo "in the while loop <br>";
+while ($i < 5)
+{
+    echo "$i";
+    echo "<br>";
+    $i++;
+}
+$j = 50;
+echo 'in the do...while loop <br>';
+do
+{
+    echo "$j";
+    echo "<br>";  
+}while ($j<5)
+?>
